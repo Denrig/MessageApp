ca28c483f50d8d321dfb2e1cf43cc436db99fdca
Boer Bogdan-Razvan <50420931+Denrig@users.noreply.github.com>
2020-06-29 22:47:43 +0300

    User controller,Register And Login Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'users#index'
+  get '/users/register' => 'users#register'
+  post '/users' => 'users#create'
+#  post '/sessions' => 'sessions#create'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_29_174107) do
+ActiveRecord::Schema.define(version: 2020_06_29_194155) do
+
+  create_table "tests", force: :cascade do |t|
+    t.string "msg1"
+    t.string "msg2"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
-    t.string "password"
+    t.string "name"
     t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
